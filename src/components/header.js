92cffa9ab9f9b51bd2700729260ae8e9be7e0f80
deindedawodu <mr.dawodu@gmail.com>
2017-02-08 15:44:48 -0500

    The first commit to kick things off

---
+++ src/components/header.js
@@ -0,0 +1,22 @@
+import React,{Component} from 'react';
+
+class Header extends Component{
+	render(){
+		return(
+     <nav className='navbar navbar-light'>
+      <ul className='nav navbar-nav'>
+       <li className='navbar-item'>
+       </li>
+         <li className='navbar-item'>
+       </li>
+         <li className='navbar-item'>
+       </li>
+      </ul>
+
+     </nav>
+		);
+	}
+}
+
+
+export default Header;
