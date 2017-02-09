f3e2bf2a31e1074143ce85a82ea5f9b7ca0fd88f
deindedawodu <mr.dawodu@gmail.com>
2017-02-09 18:54:45 -0500

    last commit

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,17 +1,3 @@
-/*import {CHANGE_AUTH} from '../actions/types';
-
-console.log('inside reducer authitication',CHANGE_AUTH);
-
-
-
-export default function(state=false, action){
-	switch(action.type) {
-		case CHANGE_AUTH:
-		 return console.log('ok',action.payload);
-	}
-	return state;
-}*/
-
 
 import {CHANGE_AUTH} from '../actions/types';
 
